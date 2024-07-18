4e4463ca22f715a56ce11e3dcdef6028687e52a9
balamurugan634 <bala060304@gmail.com>
2024-07-18 16:33:30 +0530

    feat : service page done - bala

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar'
+import Services from './pages/Services'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      {/* <Services />  yet to conditional routed*/} 
     </>
   )
 }
